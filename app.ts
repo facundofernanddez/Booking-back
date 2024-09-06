471fcbfa9ed1493a684e4fb22fae5710ef64a562
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-09-06 20:51:42 -0300

    config port and host

---
+++ app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = parseInt(process.env.PORT ?? "3000", 10);
+const HOST = process.env.HOST ?? "localhost";
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running at http://${HOST}:${PORT}`);
+});
