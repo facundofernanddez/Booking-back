906d2be874adba93d07570778ff8678df5f2454d
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2024-09-07 16:58:51 -0300

    create user model

---
+++ api/user/model.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+});
+
+userSchema.pre("save", async () => {
+  try {
+    const hashedPassword = await bcrypt.hash(this.password ?? "", 10);
+  } catch (error) {}
+});
